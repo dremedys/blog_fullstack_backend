2a49d04a3c512686280d2f494b714ab0d3b03a18
Dariga Abdikarimova <61074211+dremedys@users.noreply.github.com>
2021-06-06 05:45:41 +0500

    ok

--- blog_fullstack_backend/settings.py
+++ blog_fullstack_backend/settings.py
@@ -13,7 +13,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'blog',
     'corsheaders',
     'djoser',
